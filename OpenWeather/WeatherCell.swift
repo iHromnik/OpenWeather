a05bb3b1c5a341f3fdb8323423607423ada08934
iHromnik <hromnik@me.com>
2023-04-04 21:28:34 +0300

    add Cell prototype

---
+++ OpenWeather/WeatherCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewWeather.swift
+//  OpenWeather
+//
+//  Created by HappyRoman on 04/04/2023.
+//
+
+import UIKit
+
+class WeatherCell: UICollectionViewCell {
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+}
